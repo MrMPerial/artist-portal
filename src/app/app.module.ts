994a295d176ad7e3887a4f314019372811814ce6
MrMPerial <christopher.mitchell26@gmail.com>
2017-10-25 19:19:01 -0500

    Setup the basics (header and signup)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './signup/signup.component';
+import { ArtistHeaderComponent } from './artist-header/artist-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupComponent,
+    ArtistHeaderComponent
   ],
   imports: [
     BrowserModule,
